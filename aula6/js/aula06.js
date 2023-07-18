c43631d9fa20332d02d60ebdb05d47a27013768b
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2023-07-18 10:44:05 -0300

    Update aula06.js
    Salvei  o código js no lugar correto

--- aula6/js/aula06.js
+++ aula6/js/aula06.js
@@ -1,15 +1,38 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aula 06 de JS</title>
-</head>
-<body>
-    <h1 id='titulo'>Exercitar é preciso, senão a rotina te cansa</h1>
-    
-<!-- link para arquivo js externo -->
-<script src="js/aula06.js"></script>
-</body>
-</html>
+// comentario em linha
+
+/*
+Comentario em bloco
+*/
+
+// criar variaveis use let ou var
+let produto = 'Arroz'
+let quant = 2
+let precoUnitario = 4.99
+let precoTotal = quant * precoUnitario
+document.write(`Preço Total R$ ${precoTotal} <br>`)
+console.log(new Intl.NumberFormat('pt-BR').format(precoTotal))
+
+document.write(`Preço Total ${precoTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} <hr>`)
+
+// criar constante
+const nome = 'Edson Maia'
+const peso = 83.5
+const altura = 1.75
+const imc = peso / (altura * altura)
+
+document.write(`IMC: ${imc} <br>`)
+document.write(`IMC: ${imc.toFixed(2)} <br>`)
+
+let nota1 = 7.5
+let nota2 = 5.5
+let nota3 = 4.0
+let notaFinal = nota1 + nota2 + nota3
+document.write("Nota Final " + notaFinal+ '<br>')
+
+let compra = 1999.99
+let desconto = 145.42
+let compraComDesconto = compra - desconto
+
+document.write(compraComDesconto.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })+ '<br>')
+
+document.write(compraComDesconto + '<br>')
