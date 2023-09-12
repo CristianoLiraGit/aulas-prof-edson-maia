f90e83ad55e82b8835a00d27696df99aa959719a
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2023-09-11 22:51:02 -0300

    Update main.js
    Códigos js da aula 43

---
+++ aula43/main.js
@@ -0,0 +1,35 @@
+// Programa principal
+// POO = Programação Orientada a Objetos
+
+// classes = são modelos ou modes de objetos
+// objetos = são abstrações ou representações de "coisas" que existem no mundo real
+// objeto = instância ou objeto do tipo | objeto é um tipo de dados
+
+class Pessoa
+{
+    // atributos = variáveis ou características
+    nome // tipo String
+    idade // tipo Integer
+    peso  // tipo Float ou Real
+
+    // métodos = funções ou comportamentos
+}
+
+// criar novo objeto a partir da class, instanciar
+let pessoa1 = new Pessoa()
+let pessoa2 = new Pessoa()
+/*
+console.log(pessoa1)
+console.log(pessoa2)
+*/
+// atribuir valores para os atributos de um objeto
+pessoa1.nome = 'Cristiano'
+pessoa1.idade = 40
+pessoa1.peso = 84.5
+
+pessoa2.nome = 'Maria'
+pessoa2.idade = 63
+pessoa2.peso = 70.5
+
+console.log(pessoa1)
+console.log(pessoa2)
