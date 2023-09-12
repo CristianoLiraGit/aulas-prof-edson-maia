abe7b4300bedd387f42c5fb02366366d1f81ec15
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2023-09-11 23:27:25 -0300

    Update main.js
    Código js da aula 45

--- aula45/main.js
+++ aula45/main.js
@@ -7,32 +7,41 @@
 
 class Pessoa
 {
-    // atributos = variáveis ou características
+    //? atributos = variáveis ou características
     _nome // tipo String - o underline está indicando que os atributos são protegidos (privados)
     _idade // tipo Integer
     _peso  // tipo Float ou Real
     _altura // tipo Float ou Real
 
-    // métodos = funções ou comportamentos
+    //! métodos = funções ou comportamentos
+    // método construtor
+    constructor (nome, idade, peso, altura) {
+        this._nome = nome
+        this._idade = idade
+        this.peso = peso
+        this._altura = altura
+    }
 }
 
 // criar novo objeto a partir da class, instanciar
-let pessoa1 = new Pessoa()
-let pessoa2 = new Pessoa()
+let pessoa1 = new Pessoa('Cristiano Barbosa', 37, 85.5, 1.82)
+let pessoa2 = new Pessoa('Inácia Barbosa', 64, 63.5, 1.63)
 /*
 console.log(pessoa1)
 console.log(pessoa2)
 */
-// atribuir valores para os atributos de um objeto
-pessoa1.nome = 'Cristiano'
-pessoa1.idade = 40
-pessoa1.peso = 84.5
-pessoa1.altura = 1.76
+//! atribuir valores para os atributos de um objeto
 
-pessoa2.nome = 'Maria'
-pessoa2.idade = 63
-pessoa2.peso = 70.5
-pessoa2.altura = 1.63
+//todo - Nesta aula, foi visto que não é correto criar os objetos com valores inicialmente indefinidos e depois atribuir aqui os valores. Fere alguns princípios de segurança. Usa-se o método constructor.
+// pessoa1.nome = 'Cristiano'
+// pessoa1.idade = 40
+// pessoa1.peso = 84.5
+// pessoa1.altura = 1.76
+
+// pessoa2.nome = 'Maria'
+// pessoa2.idade = 63
+// pessoa2.peso = 70.5
+// pessoa2.altura = 1.63
 
 console.log(pessoa1)
 console.log(pessoa2)
