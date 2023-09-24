1c69ae1c8a083c096db8981a16324f9f1de900f2
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2023-09-24 11:10:38 -0300

    Create teste json files
    Criei arquivos para aprender sobre json

---
+++ teste-json/js/scripts.js
@@ -0,0 +1,24 @@
+const objs = [ 
+    {
+        nome: 'Cristiano',
+        idade: 37,
+        esta_trabalhando: true,
+        detalhes_profissão: {
+            profissão: 'Programador',
+            empresa: 'Empresa X'
+        },
+        hobbies: ['tocar guitarr', 'Correr', 'Programar'],
+    },
+
+    {
+        nome: 'Natália',
+        idade: 30,
+        esta_trabalhando: false,
+        detalhes_profissão: {
+            profissão: null,
+            empresa: null,
+        },
+        hobbies: ['Ler', 'Academia', 'Escovar cabelos'],
+    },
+    
+]
