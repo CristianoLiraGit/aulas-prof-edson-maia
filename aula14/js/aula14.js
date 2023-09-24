9a82c071be6a7754ced9bee322a1f13a1bf18a23
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2023-09-23 23:01:15 -0300

    Update aula14.js
    Fiz uma cópia da função utilizada, mudando-a para o padrão arrow function.

--- aula14/js/aula14.js
+++ aula14/js/aula14.js
@@ -73,22 +73,43 @@ btnEnviar.addEventListener('click', function (e) {
     e.preventDefault()
 })
 
-function situacaoDoPeso(imc) {
-    let situacao = ''
-    if (imc < 18.5) {
-        situacao = 'Baixo peso'	
-    } else if (imc >= 18.5 && imc <= 24.9) {
-        situacao = 'Peso normal'
-    } else if (imc >= 25 && imc <= 29.9) {
-        situacao = 'Sobrepeso'
-    } else if (imc >= 30 && imc <= 34.9) {
-        situacao = 'Obesidade I'
-    } else if (imc >= 35 && imc <= 39.9) {
-        situacao = 'Obesidade II'
-    } else if (imc >= 40) {
-        situacao = 'Obesidade III'
-    } else {
-        situacao = 'Informe seu peso corretamente'
-    }
-    return situacao
+// function situacaoDoPeso(imc) {
+//     let situacao = ''
+//     if (imc < 18.5) {
+//         situacao = 'Baixo peso'	
+//     } else if (imc >= 18.5 && imc <= 24.9) {
+//         situacao = 'Peso normal'
+//     } else if (imc >= 25 && imc <= 29.9) {
+//         situacao = 'Sobrepeso'
+//     } else if (imc >= 30 && imc <= 34.9) {
+//         situacao = 'Obesidade I'
+//     } else if (imc >= 35 && imc <= 39.9) {
+//         situacao = 'Obesidade II'
+//     } else if (imc >= 40) {
+//         situacao = 'Obesidade III'
+//     } else {
+//         situacao = 'Informe seu peso corretamente'
+//     }
+//     return situacao
+
+    /*Mesma função com arrow function*/
+
+    let situacaoDoPeso = (imc) =>{
+        let situacao = ''
+        if (imc < 18.5) {
+            situacao = 'Baixo peso'	
+        } else if (imc >= 18.5 && imc <= 24.9) {
+            situacao = 'Peso normal'
+        } else if (imc >= 25 && imc <= 29.9) {
+            situacao = 'Sobrepeso'
+        } else if (imc >= 30 && imc <= 34.9) {
+            situacao = 'Obesidade I'
+        } else if (imc >= 35 && imc <= 39.9) {
+            situacao = 'Obesidade II'
+        } else if (imc >= 40) {
+            situacao = 'Obesidade III'
+        } else {
+            situacao = 'Informe seu peso corretamente'
+        }
+        return situacao
 }
